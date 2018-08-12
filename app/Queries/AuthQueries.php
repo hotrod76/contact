43d46125b33dc1ffcb3d818976f26b620196bf2e
jason kinney <jason@jasons-mac-mini.lan>
2018-08-12 12:56:24 -0500

    login and register

--- app/Queries/AuthQueries.php
+++ app/Queries/AuthQueries.php
@@ -31,11 +31,60 @@ trait AuthQueries
 		return ['message' => 'success'];
 	}
 
+	/**
+     * Login user and create token
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Support\Collection 
+     */
 	public function login(Request $request)
 	{
 		$request->validate([
 			'email' => 'required',
 			'password' => 'required',
+			'remember_me' => 'required|boolean',
 		]);
+
+		$credentials = request(['email', 'password']);
+
+        if( !Auth::attempt( request( ['email', 'password'] ) ) ) {
+
+            return response()->json( ['message' => 'Unauthorized', 401 );
+
+        }
+
+        $user = $request->user();
+
+        $tokenResult = $user->createToken('Personal Access Token');
+
+        $token = $tokenResult->token;
+
+        if ($request->remember_me) {
+
+            $token->expires_at = Carbon::now()->addWeeks(1);
+
+        }
+
+        $token->save();
+
+        return collect([
+            'access_token' => $tokenResult->accessToken,
+            'token_type' => 'Bearer',
+            'expires_at' => Carbon::parse( $tokenResult->token->expires_at )->toDateTimeString(),
+            'user' => $request->user(),
+        ]);
+	}
+
+	public function logout()
+	{
+		if( $request->user() ) {
+
+			$request->user()->token()->revoke();
+
+			return ['message' => 'success'];
+
+		}
+
+		return ['message' => 'error'];
 	}
 }
